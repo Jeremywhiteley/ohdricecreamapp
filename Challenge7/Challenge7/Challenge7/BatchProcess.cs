c634ba557e503c4e43dcf7b3c6fb8b6cde00cce5
Brent Stineman <brent@stinemans.org>
2018-05-01 22:14:43 -0700

    adding challenge 7 code

---
+++ Challenge7/Challenge7/Challenge7/BatchProcess.cs
@@ -0,0 +1,37 @@
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using System;
+
+namespace Challenge7
+{
+    public static class BatchProcess
+    {
+        [FunctionName("BatchProcess")]
+        public static void Run([EventHubTrigger("batchhub", Connection = "eventConnectionString")]EventData[] myEventHubMessages, TraceWriter log)
+        {
+            // set up docDB client
+            var documentClient = new DocumentClient(new Uri("https://ohdrteamdb-001.documents.azure.com:443/"), "1ZHhCMRJGECdqFajFLVIYYhSQG5OIsQSo0OjFacj9b9ZQeYvKZ5nDzXwXYpBomEmgpht9v5EAnNcYDYkF4TWuA==");
+            var databaseResponse = documentClient.CreateDatabaseIfNotExistsAsync(new Database() { Id = "Challenge7DB" }).Result;
+            var database = databaseResponse.Resource as Database;
+            var databaseLink = UriFactory.CreateDatabaseUri(database.Id);
+            var documentCollectionResponse = documentClient.CreateDocumentCollectionIfNotExistsAsync(databaseLink, new DocumentCollection() { Id = "OrderHeaderDetails" }).Result;
+            var documentCollection = documentCollectionResponse.Resource as DocumentCollection;
+            var documentCollectionLink = UriFactory.CreateDocumentCollectionUri(database.Id, documentCollection.Id);
+
+            // process messages
+            foreach (EventData message in myEventHubMessages)
+            {
+                // process each message
+
+
+                // write to DocDB
+                documentClient.CreateDocumentAsync(documentCollectionLink, message).Wait();
+            }
+
+        }
+    }
+}
